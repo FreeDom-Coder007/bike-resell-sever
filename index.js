d943a0a50577b48898b73440ec9eff29f9ec47a6
FreeDom-Coder-007 <afzalsharif623@gmail.com>
2022-11-27 17:50:34 +0600

    seller's product getting api added

--- index.js
+++ index.js
@@ -1,20 +1,114 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 //Middle Wares
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.BIKE_RESALE_USER}:${process.env.BIKE_RESALE_PASSWORD}@cluster0.gijesb3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
+  try{
+    const advertisedProductsCollection = client.db('bikeReSale').collection('advertisedProducts')
+    const categoriesCollection = client.db('bikeReSale').collection('categories')
+    const productsCollection = client.db('bikeReSale').collection('products')
+    const usersCollection = client.db('bikeReSale').collection('users')
+    const bookingsCollection = client.db('bikeReSale').collection('bookings')
+    
 
+    app.get('/advertised-products', async(req, res) => {
+       const query = {}
+       const products = await advertisedProductsCollection.find(query).toArray()
+       res.send(products)
+    })
+
+    app.get('/product-categories', async(req, res) => {
+       const query = {}
+       const categories = await categoriesCollection.find(query).toArray()
+       res.send(categories)
+    })
+
+    // Products Collection api 
+    app.get('/products', async(req, res) => {
+       const categoryName = req.query.categoryName
+       const query = {category_name: categoryName}
+       const result = await productsCollection.find(query).toArray()
+       res.send(result)
+    })
+    app.get('/products', async(req, res) => {
+       const email = req.query.email
+       const query = {sellerEmail: email}
+       const result = await productsCollection.find(query).toArray()
+       res.send(result)
+    })
+    app.get('/category/:id', async(req, res) => {
+       const id = req.params.id
+       const filter = {_id: ObjectId(id)}
+       const result = await categoriesCollection.findOne(filter)
+       res.send(result)
+    })
+
+    app.post('/users', async(req, res) => {
+       const user = req.body
+       const email = req.query.email 
+       const query = {email: email}
+       const alredySavedUser = await usersCollection.find(query).toArray()
+       if(alredySavedUser.length){
+          return res.send({acknowledged: false});
+       }
+       const result = await usersCollection.insertOne(user)
+       res.send(result)
+    })
+    app.get('/users/:email', async(req, res) => {
+       const email = req.params.email
+       const query = {email: email}
+       const user = await usersCollection.findOne(query)
+       res.send(user)
+    })
+
+    app.post('/bookings', async(req, res) => {
+       const booking = req.body
+       const result = await bookingsCollection.insertOne(booking)
+       res.send(result)
+    })
+    app.get('/bookings', async(req, res) => {
+       const email = req.query.email
+       const filter = {email: email}
+       const result = await bookingsCollection.find(filter).toArray()
+       res.send(result)
+    })
+    app.get('/bookings/:id', async(req, res) => {
+       const id = req.params.id
+       const filter = {_id: ObjectId(id)}
+       const result = await bookingsCollection.findOne(filter)
+       res.send(result)
+    })
+
+    app.post('/create-payment-intent', async(req, res) => {
+       const product = req.body;
+       const price = product.resellPrice;
+       const amount = price * 100
+
+       const paymentIntent = await stripe.paymentIntents.create({
+          currency: 'usd',
+          amount: amount,
+          "payment_method_types": [
+            "card"
+          ], 
+       })
+       res.send({
+          clientSecret: paymentIntent.client_secret,
+       });
+    })
+
+  }
+  finally{}
 }
 
 run()
